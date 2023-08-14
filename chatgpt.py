3b54f493b2f2d82acd7c751042baca6e5cf07ccd
THEPINEAPPLESG <steven@thepineapple.io>
2023-08-14 10:19:11 +0800

    update

--- chatgpt.py
+++ chatgpt.py
@@ -8,7 +8,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.llms import OpenAI
-from langchain.vectorstores import Chroma
+#from langchain.vectorstores import Chroma
 import constants
 
 app = Flask(__name__)
